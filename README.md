5728376c524ead01cab33db2f1e04c624c231864
Jack Goodall <jack@safecube.com>
2023-04-11 13:31:16 -0400

    fix: hide comments window on quitting shorts

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 Boosts for Arc Browser by [therosbif](https://arcboosts.com/@rosbif)
 
-## [<img src="./yt-shorts-side-by-side/logo.png" alt="logo" height="24" /> YouTube Shorts Side Comments](./yt-shorts-side-by-side/)
+## [<kbd><img src="./yt-shorts-side-by-side/logo.png" alt="logo" height="24" /></kbd> YouTube Shorts Side Comments](./yt-shorts-side-by-side/)
 
 View YouTube Shorts comments side-by-side with the video.
 
 Opens comments automatically and reloads for every video.
 
-Tested with URL `www.youtube.com/shorts`
+Set URL to `www.youtube.com`
 
 ![screenshot](./yt-shorts-side-by-side/screenshot.png)
 
--- yt-shorts-side-by-side/content.js
+++ yt-shorts-side-by-side/content.js
@@ -1,13 +1,17 @@
-const buttonClasses = "yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-l yt-spec-button-shape-next--icon-button"
+const buttonClasses =
+  "yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-l yt-spec-button-shape-next--icon-button";
 
 function checkVisible(elm) {
-  var rect = elm.getBoundingClientRect();
-  var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+  const rect = elm.getBoundingClientRect();
+  const viewHeight = Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight
+  );
   return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
 }
 
 function getButton() {
-  const divs = document.querySelectorAll('[id="comments-button"]')
+  const divs = document.querySelectorAll('[id="comments-button"]');
   for (const div of divs) {
     if (checkVisible(div)) {
       const buttons = div.getElementsByClassName(buttonClasses);
@@ -17,18 +21,36 @@ function getButton() {
   return null;
 }
 
-let oldHref = '';
-const body = document.querySelector("body");
-const observer = new MutationObserver(mutations => {
-    mutations.forEach(() => {
-    if (oldHref !== document.location.href) {
-      oldHref = document.location.href;
+function updateCommments() {
+  setTimeout(() => {
+    const button = getButton();
+    button?.click();
+  }, 0);
+}
 
-      setTimeout(() => {
-        const button = getButton()
-        button?.click();
-      }, 0)
+updateCommments();
+
+let oldPathname = "";
+const body = document.querySelector("body");
+
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((m) => {
+    const pathname = document.location.pathname;
+
+    if (oldPathname === pathname) {
+      return;
     }
+
+    if (
+      oldPathname !== "" &&
+      pathname.startsWith("/shorts") !== oldPathname.startsWith("/shorts")
+    ) {
+      window.location.reload();
+      return;
+    }
+
+    oldPathname = pathname;
+    updateCommments();
   });
 });
-observer.observe(body, { childList: true, subtree: true });
+observer.observe(body, { childList: true });
